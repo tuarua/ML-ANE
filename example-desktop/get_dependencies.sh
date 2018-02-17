182cc4c1a1114fa910886f16e9cb3dd2b7e83310
Tua Rua <el111@yahoo.com>
2018-02-17 19:02:28 +0000

    Update get_dependencies.sh

--- example-desktop/get_dependencies.sh
+++ example-desktop/get_dependencies.sh
@@ -2,4 +2,4 @@
 
 wget -O ../native_extension/ane/CommonDependencies.ane https://github.com/tuarua/Swift-OSX-ANE/releases/download/2.2.0/CommonDependencies.ane?raw=true
 
-wget -O ../native_extension/ane/MLANE.ane https://github.com/tuarua/ML-ANE/releases/download/0.0.1/ARANE.ane?raw=true
+wget -O ../native_extension/ane/MLANE.ane https://github.com/tuarua/ML-ANE/releases/download/0.0.2/ARANE.ane?raw=true
