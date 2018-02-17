1ef3d1417805c29cf4f81f97aefaeba0181fda8b
Tua Rua <el111@yahoo.com>
2018-02-17 23:52:42 +0000

    Update get_ios_dependencies.sh

--- example-mobile/get_ios_dependencies.sh
+++ example-mobile/get_ios_dependencies.sh
@@ -4,7 +4,7 @@ wget https://github.com/tuarua/Swift-IOS-ANE/releases/download/2.2.0/ios_depende
 unzip -u -o ios_dependencies.zip
 rm ios_dependencies.zip
 
-wget https://github.com/tuarua/AR-ANE/releases/download/0.0.2/ios_dependencies.zip
+wget https://github.com/tuarua/ML-ANE/releases/download/0.0.2/ios_dependencies.zip
 unzip -u -o ios_dependencies.zip
 rm ios_dependencies.zip
 
