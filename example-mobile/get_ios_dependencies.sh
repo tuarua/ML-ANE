853384acf11ea54f82df537e1da7777dd462a812
Eoin Landy <el111@yahoo.com>
2018-03-31 14:53:19 +0100

    0.0.6

--- example-mobile/get_ios_dependencies.sh
+++ example-mobile/get_ios_dependencies.sh
@@ -3,12 +3,12 @@
 rm -r ios_dependencies/device
 rm -r ios_dependencies/simulator
 
-wget https://github.com/tuarua/Swift-IOS-ANE/releases/download/2.3.0/ios_dependencies.zip
+wget https://github.com/tuarua/Swift-IOS-ANE/releases/download/2.4.0/ios_dependencies.zip
 unzip -u -o ios_dependencies.zip
 rm ios_dependencies.zip
 
-wget https://github.com/tuarua/ML-ANE/releases/download/0.0.5/ios_dependencies.zip
+wget https://github.com/tuarua/ML-ANE/releases/download/0.0.6/ios_dependencies.zip
 unzip -u -o ios_dependencies.zip
 rm ios_dependencies.zip
 
-wget -O ../native_extension/ane/MLANE.ane https://github.com/tuarua/ML-ANE/releases/download/0.0.5/MLANE.ane?raw=true
+wget -O ../native_extension/ane/MLANE.ane https://github.com/tuarua/ML-ANE/releases/download/0.0.6/MLANE.ane?raw=true
